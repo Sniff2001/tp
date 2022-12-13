c4527c9a2a6b299cba98e13f03bb4c044aa8059a
eilifso <e.s.oyre@astro.uio.no>
2022-12-13 13:14:52 +0100

    [DEV] Adds grid cell binary search.

--- src/Interpolations.jl
+++ src/Interpolations.jl
@@ -28,15 +28,36 @@ function grid(mesh       ::Mesh,
     i = locateCell(mesh.xCoords, x)
     j = locateCell(mesh.yCoords, y)
     k = locateCell(mesh.zCoords, z)
-    # Interpolate
-    B = zeros(numDims) # The interpolated magnetic field
-    E = zeros(numDims) # The interpolated electric field
-    for l = 1:numDims # loop over dimensions
-        B[l] = interpolator(mesh.bField[l, :, :, :], (i,j,k), (x,y,z))
-        E[l] = interpolator(mesh.eField[l, :, :, :], (i,j,k), (x,y,z))
-    end # loop over dimensions
-    return B, E
+
+    B, E = interpolator(mesh, (i,j,k), (x,y,z))
     
+    return B, E
+
 end # function grid
 
+
+"""
+    locateCell(coords, point)
+
+Finds the position of `point` in the vector `coords` using binary search.
+Returns its lower neighbour.
+"""
+function locateCell(coords::Vector{wpFloat},
+                    point ::Number
+                    )
+    # Initial bounds
+    low = 1
+    high = length(coords)
+    while  (high - low) > 1 # While we haven't found the cell
+        mid  = convert(wpInt, (high - low)/2)
+        if point > coords[mid]
+            low = mid
+        else
+            high = mid
+        end # if
+    end # while
+    return low 
+end # function locateCell
+
+    
 end # module Interpolations

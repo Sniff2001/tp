92af9ec2901f8538fbf293fef05df4624607c669
eilifso <e.s.oyre@astro.uio.no>
2022-12-14 17:22:25 +0100

    [ADD] Adds unit-testing-script.

---
+++ tests/unitTesting.jl
@@ -0,0 +1,18 @@
+
+#-------------------------------------------------------------------------------
+#
+# Created 13.12.22
+# Author: e.s.oyre@astro.uio.no
+#
+# Last edited: 13.12.22
+#
+#-------------------------------------------------------------------------------
+# Script for running tests.
+#-------------------------------------------------------------------------------
+
+using Test
+using TestInterpolations
+
+@testset verbose = true "Interpolations" begin
+    testtrilinear(true)
+end

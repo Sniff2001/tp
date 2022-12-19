c012251e614ae4510667acbb9f829c3eba71b7e2
eilifso <e.s.oyre@astro.uio.no>
2022-12-19 15:39:25 +0100

    [ADD] Adds Constants.jl.

---
+++ src/Constants.jl
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+#
+# Created 19.12.22
+# Author: e.s.oyre@astro.uio.no
+#
+# Last edited: 19.12.22
+#
+#-------------------------------------------------------------------------------
+#
+#                Constants.jl
+#
+#-------------------------------------------------------------------------------
+# Module containing physical constants
+#-------------------------------------------------------------------------------
+
+module Constants
+
+k_B         = 1.380649e-23 # J/K ....  Boltzmann's constant
+c           = 299792458 # m/s ........ Speed of light in vacuum
+cSquaredInv = 1/c^2     # s^2 m^-2 ... Inverse of the light speed squared
+
+end # module Constants

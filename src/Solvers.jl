cb53589a718ff541eea3bdebe0847b85c018ea9d
eilifso <e.s.oyre@astro.uio.no>
2022-12-19 15:38:56 +0100

    [DEV] Adds Vay (2008)-pusher.

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -1,10 +1,9 @@
 #-------------------------------------------------------------------------------
 #
 # Created 02.12.22
-# By Eilif @RoCS UiO.
-# email: e.s.oyre@astro.uio.no
+# Author: e.s.oyre@astro.uio.no
 #
-# Last edited: 07.12.22
+# Last edited: 19.12.22
 #
 #-------------------------------------------------------------------------------
 #
@@ -16,15 +15,63 @@
 module Solvers
 
 # Standard library
-using LinearAlgebra: cross
+using LinearAlgebra: ×, ⋅, norm
 # Internal modules
 using Particles: specieTable
+using Constants: c, cSquaredInv
 
 
-function fullOrbit(pos, vel, specie, bField, eField)
+function fullOrbit(pos, vel, specie, bField, eField, dt, scheme)
     mass   = specieTable[specie, 1]
     charge = specieTable[specie, 2]
-    acc = charge/mass * (eField + cross(vel, bField))
-end
+    acc = charge/mass * (eField + vel × bField)
+    pos, vel = scheme(pos, vel, acc, dt)
+    return pos, vel
+end # funcion fullOrbit
 
-end
+
+"""
+    vay(pos, vel, specie, bField, eField, dt, scheme)
+
+Implementation of the relativistic Vay pusher. Adapted from J.-L. Vay (2008)
+"""
+function vay(pos, vel, specie, bField, eField, dt, scheme)
+    mass   = specieTable[specie, 1]
+    charge = specieTable[specie, 2]
+    # Some factors which use is repeated
+    factor1 = charge*dt/2mass
+    factor2 = 2factor1
+
+    v = norm(vel)                  # Speed of the particle
+    γ = √(1/(1 - v^2*cSquaredInv)) # The relativistic gamma-factor
+    u = γ.*vel                     # The relativistic velocity
+
+    # Compute half-step in velocity
+    uHalf = u + factor1*(eField + vel × bField)
+
+    # Compute various quantities used to get the full step in velcity
+    # See Vay 2008 for details
+    uPrime = uHalf + (factor1*eField)
+    τ = factor1*bField
+    uPrimeNorm = norm(uPrime)
+    τNorm = norm(τ)
+    uStar = uPrime ⋅ τ/c
+    γPrime = √(1 - uPrimeNorm^2*cSquaredInv)
+    σ = γPrime^2 - τNorm^2
+    # Compute the gamma-factor for full step
+    γNext = √(0.5(σ + √(σ^2 + 4(τNorm^2*uPrimeNorm^2))))
+    t = τ/γNext
+    s = 1/(1 + norm(t)^2)
+    # Finally, compute the full step in velocity
+    uNext = s*(uPrime + (uPrime ⋅ t)t + uPrime × t)
+
+    # Get the non-relativistic velocity and position
+    velNext = uNext/γNext
+    posNext = velNext*dt  # Simple integration of velocity
+
+    return posNext, velNext
+    
+end # Function vay
+
+
+end # module Solvers

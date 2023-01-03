e1cdd5ddd2f615ef2ca2b286a8f67f970b3226a2
eilifso <e.s.oyre@astro.uio.no>
2023-01-03 19:09:09 +0100

    [ADD] Adds TestMeshes-module.

---
+++ tests/modules/TestMeshes.jl
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Created 02.01.23
+# Author: e.s.oyre@astro.uio.no
+#-------------------------------------------------------------------------------
+#
+#             TestMeshes.jl
+#
+#-------------------------------------------------------------------------------
+# Module containing test of the Meshes-module.
+#-------------------------------------------------------------------------------
+
+module TestMeshes
+
+using Test
+
+using WorkingPrecision: wpInt, wpFloat
+using Meshes
+
+"""
+There are currently no Meshes-specific unit-tests.
+
+See `testtrilinear` in TestInterpolations.jl and testExBdrift.jl for indirect 
+tests.
+"""

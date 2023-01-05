3ea03a861d5bca42536665e02eaea15ccf969051
eilifso <e.s.oyre@astro.uio.no>
2023-01-05 11:22:43 +0100

    [DEV] Moves the Vay algorithm to `Schemes` and adds the solver `relFullOrbitExpLeapFrog`.

--- src/Schemes.jl
+++ src/Schemes.jl
@@ -11,16 +11,83 @@
 
 module Schemes
 
-function euler(pos, vel, acc, dt)
+
+function euler(pos::Vector{wpFloat},
+               vel::Vector{wpFloat}, 
+               acc::Vector{wpFloat},
+               dt)
     nextPos = @. pos + vel * dt
     nextVel = @. vel + acc * dt
     return nextPos, nextVel
-end
+end # function euler
 
-function eulerCromer(pos, vel, acc, dt)
+
+function eulerCromer(pos::Vector{wpFloat},
+                     vel::Vector{wpFloat}, 
+                     acc::Vector{wpFloat},
+                     dt)
     nextVel = @. vel + acc * dt
     nextPos = @. pos + nextVel * dt
     return nextPos, nextVel
-end
+end # function eulerCromer
 
-end
+
+function positionHalfStep(pos::Vector{wpFloat},
+                          vel::Vector{wpFloat},
+                          dt
+                          )
+    return pos + 0.5dt*vel
+end # function positionHalfStep
+
+
+"""
+    vay(pos, vel, specie, bField, eField, dt, scheme)
+
+Implementation of the Vay pusher. For integrating the relativistic Lorentz
+eqution. Adapted from J.-L. Vay (2008). This is only the second step of the
+implementation, where the relativistic velocity is advanced a time step. The
+first step involves advancing the position half a time step using
+`positionHalfStep`, and in the last step the position is advanced its final 
+half using the same function but with the new relativistic velocity.
+"""
+function vay(vel   ::Vector{wpFloat},
+             bField::Vector{wpFloat},
+             eField::Vector{wpFloat},
+             mass  ::wpFloat,
+             charge::wpFloat,
+             dt    ::wpFloat
+             )
+    # Some factors which use is repeated
+    factor1 = 0.5charge*dt/mass
+
+    v = norm(vel)                  # Speed of the particle
+    γ = √(1/(1 - v^2*cSqrdInv)) # The relativistic gamma-factor
+    u = γ * vel                   # The relativistic velocity
+
+    # Compute half-step in velocity
+    uHalf = u + factor1*(eField + vel × bField)
+
+    # Compute various quantities used to get the full step in velcity
+    # See Vay 2008 for details
+    uPrime = uHalf + factor1*eField
+    τ = factor1*bField
+    uPrimeNorm = norm(uPrime)
+    τNorm = norm(τ)
+    uStar = uPrime ⋅ τ/c
+    γPrime = √(1 + uPrimeNorm^2*cSqrdInv)
+    σ = γPrime^2 - τNorm^2
+    # Compute the gamma-factor for full step
+    γNext = √(0.5(σ + √(σ^2 + 4(τNorm^2 + uStar^2))))
+    t = τ/γNext
+    s = 1/(1 + norm(t)^2)
+    # Finally, compute the full step in velocity
+    uNext = s*(uPrime + (uPrime ⋅ t)t + uPrime × t)
+
+    # Get the non-relativistic velocity and position
+    velNext = uNext/γNext
+    
+    return velNext
+end # function vayAdvanceVel
+
+
+end # module schemes
--- src/Solvers.jl
+++ src/Solvers.jl
@@ -45,20 +45,13 @@ function fullOrbit(pos         ::Vector{wpFloat},
 end # funcion fullOrbit
 
 
-"""
-    vay(pos, vel, specie, bField, eField, dt, scheme)
-
-Implementation of the Vay pusher. For integrating the relativistic Lorentz
-eqution. Adapted from J.-L. Vay (2008). This is only the second step of the
-"""
-function vay(pos         ::Vector{wpFloat},
-             vel         ::Vector{wpFloat},
-             specie      ::wpInt,
-             mesh        ::Mesh,
-             dt          ::wpFloat,
-             interpolator::Function
-             scheme      ::Function,
-             )
+function relFullOrbitExplLeapFrog(vel         ::Vector{wpFloat},
+                                  specie      ::wpInt,
+                                  mesh        ::Mesh,
+                                  dt          ::wpFloat,
+                                  interpolator::Function
+                                  scheme      ::Function,
+                                  )
     # Extract particle mass and charge
     mass   = specieTable[specie, 1]
     charge = specieTable[specie, 2]
@@ -66,7 +59,7 @@ function vay(pos         ::Vector{wpFloat},
     #
     # Step 1: Evaluate half-step in time for position
     #
-    posHalf = pos + 0.5dt*vel
+    posHalf = positionHalfStep(pos, vel, dt)
     # Interpolate fields to this location
     bField, efield = Interpolations.grid(mesh,
                                          interpolator,
@@ -75,41 +68,14 @@ function vay(pos         ::Vector{wpFloat},
     # 
     # Step 2: Evaluate full time step in velocity, which is shceme-dependent.
     #
-    # Some factors which use is repeated
-    factor1 = 0.5charge*dt/mass
-
-    v = norm(vel)                  # Speed of the particle
-    γ = √(1/(1 - v^2*cSqrdInv)) # The relativistic gamma-factor
-    u = γ * vel                   # The relativistic velocity
-
-    # Compute half-step in velocity
-    uHalf = u + factor1*(eField + vel × bField)
-
-    # Compute various quantities used to get the full step in velcity
-    # See Vay 2008 for details
-    uPrime = uHalf + factor1*eField
-    τ = factor1*bField
-    uPrimeNorm = norm(uPrime)
-    τNorm = norm(τ)
-    uStar = uPrime ⋅ τ/c
-    γPrime = √(1 + uPrimeNorm^2*cSqrdInv)
-    σ = γPrime^2 - τNorm^2
-    # Compute the gamma-factor for full step
-    γNext = √(0.5(σ + √(σ^2 + 4(τNorm^2 + uStar^2))))
-    t = τ/γNext
-    s = 1/(1 + norm(t)^2)
-    # Finally, compute the full step in velocity
-    uNext = s*(uPrime + (uPrime ⋅ t)t + uPrime × t)
-
-    # Get the non-relativistic velocity and position
-    velNext = uNext/γNext
-
+    velNext = scheme(vel, bField, eField, mass, charge, dt)
+    
     #
     # Step 3: Evaluate second half of time step in position
     # 
-    posNext = posHalf + 0.5dt*velNext
+    posNext =positionHalfStep(posHalf, velNext, dt)
 
     return posNext, velNext
-end
+end # function relFullOrbitExpLeapFrog
 
 end # module Solvers

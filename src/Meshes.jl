6479600a0f3ce90e9800cc7ce509cf9b3916d0d6
eilifso <e.s.oyre@astro.uio.no>
2023-01-11 17:23:30 +0100

    [DEV][ADD] Adds grad-B to the mesh, with computation on construction.

--- src/Meshes.jl
+++ src/Meshes.jl
@@ -12,6 +12,7 @@
 module Meshes
 
 using WorkingPrecision: wpFloat, wpInt
+using Schemes:          derivate4thOrder, derivateCentral, ∇, norm4
 
 # Simple mesh
 export Mesh
@@ -20,37 +21,23 @@ export Mesh
 # Structs     #
 #-------------#-----------------------------------------------------------------
 struct Mesh
-    bField ::AbstractArray{wpFloat} # The magnetic field
-    eField ::AbstractArray{wpFloat} # The eletric field
+    bField ::AbstractArray{wpFloat}   # The magnetic field
+    eField ::AbstractArray{wpFloat}   # The eletric field
+    ∇B     ::Vector{Array{wpFloat, 3}} # The gradient of the magnetic field
     xCoords::Vector{wpFloat} # The cartesian x-coordinates of the grid points
     yCoords::Vector{wpFloat} # The cartesian x-coordinates of the grid points
     zCoords::Vector{wpFloat} # The cartesian x-coordinates of the grid points
 
     # Constructors
     #--------------------------------------------------------------------------
-
-    function Mesh(bField ::Array{wpFloat, 2},
-                  eField ::Array{wpFloat, 2}
-                  )
-        xCoords = LinRange(0,1, size(bField)[2])
-        return new(bField, eField, xCoords)
-    end # constructor 
-
-    function Mesh(bField ::Array{wpFloat, 3},
-                  eField ::Array{wpFloat, 3}
-                  )
-        xCoords = LinRange(0,1, size(bField)[2])
-        yCoords = LinRange(0,1, size(bField)[3])
-        return new(bField, eField, xCoords, yCoords)
-    end # constructor 
-
     function Mesh(bField ::Array{wpFloat, 4},
-                  eField ::Array{wpFloat, 4}
+                  eField ::Array{wpFloat, 4},
+                  ∇B     ::Array{wpFloat, 3},
+                  xCoords::Vector{wpFloat},
+                  yCoords::Vector{wpFloat},
+                  zCoords::Vector{wpFloat}
                   )
-        xCoords = LinRange(0,1, size(bField)[2])
-        yCoords = LinRange(0,1, size(bField)[3])
-        zCoords = LinRange(0,1, size(bField)[4])
-        return new(bField, eField, xCoords, yCoords, zCoords)
+        return new(bField, eField, ∇B, xCoords, yCoords, zCoords)
     end # constructor
 
     function Mesh(bField ::Array{wpFloat, 4},
@@ -59,8 +46,45 @@ struct Mesh
                   yCoords::Vector{wpFloat},
                   zCoords::Vector{wpFloat}
                   )
-        return new(bField, eField, xCoords, yCoords, zCoords)
+        dx = xCoords[2] - xCoords[1]
+        dy = yCoords[2] - yCoords[1]
+        dz = zCoords[2] - zCoords[1]
+        ∇B = ∇(norm4(bField), dx, dy, dz, derivate4thOrder) 
+        return new(bField, eField, ∇B, xCoords, yCoords, zCoords)
     end # constructor
+
+    function Mesh(bField ::Array{wpFloat, 4},
+                  eField ::Array{wpFloat, 4}
+                  )
+        xCoords = LinRange(0,1, size(bField)[2])
+        yCoords = LinRange(0,1, size(bField)[3])
+        zCoords = LinRange(0,1, size(bField)[4])
+        dx = xCoords[2] - xCoords[1]
+        dy = yCoords[2] - yCoords[1]
+        dz = zCoords[2] - zCoords[1]
+        ∇B = ∇(norm4(bField), dx, dy, dz, derivate4thOrder) 
+        return new(bField, eField, ∇B, xCoords, yCoords, zCoords)
+    end # constructor
+
+    function Mesh(bField ::Array{wpFloat, 3},
+                  eField ::Array{wpFloat, 3}
+                  )
+        xCoords = LinRange(0,1, size(bField)[2])
+        yCoords = LinRange(0,1, size(bField)[3])
+        dx = xCoords[2] - xCoords[1]
+        dy = yCoords[2] - yCoords[1]
+        ∇B = ∇(bField, dx, dy, derivate4thOrder) # Won't work. need norm3
+        return new(bField, eField, ∇B, xCoords, yCoords)
+    end # constructor 
+
+    function Mesh(bField ::Array{wpFloat, 2},
+                  eField ::Array{wpFloat, 2}
+                  )
+        xCoords = LinRange(0,1, size(bField)[2])
+        dx = xCoords[2] - xCoords[1]
+        ∇B = derivateCentral(bField, dx, (1,0,0)) # Won't work. need norm2
+        return new(bField, eField, ∇B, xCoords)
+    end # constructor 
 end # Struct
 #-------------------------------------------------------------------------------
 

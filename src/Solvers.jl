2c112490d3841655b1020dee8512dcfd5afb8d1c
eilifso <e.s.oyre@astro.uio.no>
2023-01-11 09:29:11 +0100

    [DEV][ADD] Adds GCA solver (without grad-B implementation).

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -79,4 +79,73 @@ function relFullOrbitExplLeapFrog(pos         ::Vector{wpFloat},
     return posNext, velNext
 end # function relFullOrbitExpLeapFrog
 
+
+function GCA(pos         ::Vector{wpFloat},
+             vel         ::Vector{wpFloat},
+             specie      ::wpInt,
+             mesh        ::Mesh,
+             dt          ::wpFloat,
+             interpolator::Function,
+             scheme      ::Function
+             )
+    # Interpolate fields to this location
+    fields   = grid(mesh, interpolator, pos)
+    E        = fields[1] # The magnetic field
+    B        = fields[2] # The electric field
+    cellIdxi = fields[3] # Cell corner indexes in mesh. Corresponding to the 
+    cellIdxj = fields[4] #  position of the particle.
+    cellIdxk = fields[5]
+    
+    # Compute the gradient of the magnetic field strength
+    ∇B = magneticFieldStrengthGradient(mesh, cellidxi, cellidxj, cellIdxk)
+
+    # Extract particle mass and charge
+    m = specieTable[specie, 1]
+    q = specieTable[specie, 2]
+    
+    v⟂ = vel[4]        # Particle velocity perpendicular to the magne. field
+    v∥ = vel[5]        # Particle velocity parallel to the magnetic field
+    μ  = vel[6]        # The magnetic moment
+    |B| = norm(bField) # The magnetic field strength
+    b = bField/|B|     # An unit vector pointing in the direction of the
+                       #  magnet field
+    
+    # Electric field component parallel to the magnetic field
+    E∥ = eField ⋅ b 
+    
+    # Compute the acceleration 
+    a∥  = (q*E∥ - μ*b⋅∇B)/m # along the magnetic field lines
+    acc = a∥*b              # The vector
+    # With spatially changing fields, the velocity at this point will not be the
+    # same as the last, independent of time.
+    velhere = v∥*b + b/|B| × (-c*E + μ*c/q*∇B)
+    
+    # Use integration scheme to find velocities at the next time step
+    v∥next           = scheme(v∥, a∥, dt)
+    posNext, velNext = scheme(pos, velhere, acc, dt) # Use v∥next? Will
+    # essentially be used if the scheme is euler cromer since a∥ is in acc.
+    # norm(velNext) - norm(velhere) should equal v∥next
+    #or just? posNext, v = scheme(pos, vel[1:3], acc, dt)
+    
+    # Compute some auxiliary quantities
+    v⟂next = √(norm(v)^2 - v∥next^2) 
+    μnext = m*v∥next^2/(2|B|) #  (should be constant for all times)
+    # Maybe μ should be forced constant and kept as a parameter to this solver.
+    #   This would require a change in the implementation of solvers and
+    #   Patch.run!, where e.g. the particle type is passed to solver. Or that
+    #   run! is passed with the particle type, not the patch, such that one may
+    #   define different run methods depending on the particle type. 
+
+    velNext = [v[1], v[2], v[3], v⟂next, v∥next, μnext]
+    return posNext velNext
+
+
+
+end # function GCA
+
+
+function magneticFieldStrengthGradient(mesh, cellIdxI, cellIdxJ, cellIdxK)
+    return [0.0, 0.0, 0.0]
+end # function magneticFieldStrengthGradient
+
 end # module Solvers

b185c126ad00a09c799fbd413a874c09b5b75744
eilifso <e.s.oyre@astro.uio.no>
2023-01-18 12:04:07 +0100

    [DEV][ADD] Adds function for creating vector field with normally distributed z-comp.

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -106,4 +106,63 @@ function norm4(field::Array{wpFloat, 4},
 end # function norm4
 
 
+#----------------#
+# Distributions  #
+#----------------#--------------------------------------------------------------
+function normaldistr(x, μ=0, σ=√2)
+    return @.  1/(σ*√2π)*exp(-0.5((x-μ)/σ)^2)
+end # normaldistr
+
+#------------------------------#
+# Vector potential generation  #
+#-------------------------------------------------------------------------------
+"""
+    normal3Donlyz((x0, y0, z0), 
+                  (xf, yf, zf), 
+                  (nx, ny, nz), 
+                  (μx, μy), 
+                  (σx, σy),
+                  amplitude
+                  )
+According to given spatial domain, resolution, expectationvalue, standard
+deviation and amplitude, creates a vector field who's z-component is normally
+distributed in x and y according to the formula fz(i, j) = amplitude *
+fx(i)fy(j), where fx and fy are normal distributions in x, and y with
+expectation value and std equal to μx, μy, σx, σy, respectively. 
+"""
+function normal3Donlyz((x0, y0, z0)::Tuple{wpFloat, wpFloat, wpFloat},
+                       (xf, yf, zf)::Tuple{wpFloat, wpFloat, wpFloat},
+                       (nx, ny, nz)::Tuple{wpInt, wpInt, wpInt},
+                       (μx, μy)    ::Tuple{wpFloat, wpFloat},
+                       (σx, σy)    ::Tuple{wpFloat, wpFloat},
+                       amplitude   ::wpFloat
+                       )
+    # Create spatial axes and find the grid sizes
+    xx = collect(LinRange(x0, xf, nx))
+    dx = xx[2] - xx[1]
+    yy = collect(LinRange(y0, yf, ny))
+    dy = yy[2] - yy[1]
+    # Account for single point in the z-axis
+    if nz == 1
+        zz = [z0]
+        dz = 0.
+            else
+        zz = collect(LinRange(z0, zf, nz))
+        dz = zz[2] - zz[1]
+    end
+    # Initialise the vector field
+    ndims = 3
+    A = zeros(ndims, nx, ny, nz)
+    # Evaluate the z-component of the vecor field to be normally distributed in
+    # the x and y dimensions.
+    for i = 1:nx
+        for j = 1:ny
+            A[3,i,j,:] .= amplitude * 
+                normaldistr(xx[i], μx, σx) * normaldistr(yy[j], μy, σy)
+        end
+    end
+    return (xx, yy, zz), (dx, dy, dz), A
+end # function normal3donlyz
+
+
 end # module utilities

811fecda52831d692b3a1e489d5d1309b3476cdd
eilifso <e.s.oyre@astro.uio.no>
2023-04-28 11:05:43 +0200

    [SYN] Correct typo in test description.

--- tests/Jenkins.jl
+++ tests/Jenkins.jl
@@ -42,7 +42,7 @@ verbose = 4
         @testset verbose = true "Dipole" begin
             include("experiments/dipoleloop.jl")
         end # testset dipole
-        @testset verbose = true "Speiser (1995)" begin
+        @testset verbose = true "Speiser (1965)" begin
             include("experiments/speiser1965.jl")
         end # testset Speiser 1965
 

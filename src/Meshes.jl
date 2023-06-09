cc85f2ba1ed6ec42efce5415d568567e6546c4c1
eilifso <e.s.oyre@astro.uio.no>
2023-06-09 16:08:02 +0200

    [ADD] Add `Mesh` constructor for Bifrost snapshots.

--- src/Meshes.jl
+++ src/Meshes.jl
@@ -12,9 +12,16 @@
 module Meshes
 
 using LinearAlgebra:    ×
+using Random:           MersenneTwister
+
+using Bifrost
+
 using WorkingPrecision: wpFloat, wpInt
-using Schemes:          derivate4thOrder, derivateCentral, ∇
+using Schemes:          derivate4thOrder, derivateCentral, ∇, derivateUpwind
 using Utilities:        norm4
+using Constants
+using Schemes
+
 
 # Simple mesh
 export Mesh
@@ -69,7 +76,9 @@ struct Mesh
         ∇B, ∇b̂, ∇ExB = compute∇s(bField, eField,
                                  xCoords,
                                  yCoords,
-                                 zCoords)
+                                 zCoords,
+                                 derivateUpwind
+                                 )
         return new(bField, eField, ∇B, ∇b̂, ∇ExB,
                    xCoords, yCoords, zCoords, 
                    domain, numdims)
@@ -125,6 +134,208 @@ struct Mesh
         # Won't work. missing ∇b̂ and ∇ExB
         return new(bField, eField, ∇B, xCoords, numdims)
     end # constructor 
+
+    """
+         Using Bifrost input
+    """
+    function Mesh(
+        expname::String,
+        expdir ::String,
+        snap   ::Integer,
+        )
+
+        numdims = 3
+        #-----------------------------------------------------------------------
+        # Load Bifrost-snap
+
+        # Parse filenames
+        snapstr = "_$snap"
+        basename = string(expdir, "/", expname, "_$snap")
+        idl_filename = string(basename, ".idl")
+        snap_filename = string(basename, ".snap")
+        aux_filename = string(basename, ".aux")
+        mesh_filename = string(expdir, "/", expname, ".mesh")
+
+        br_mesh = BifrostMesh(mesh_filename)
+        br_params = br_read_params(idl_filename)
+        br_snap = br_load_snapdata(snap_filename, br_params)
+        br_aux = br_load_auxdata(aux_filename, br_params)
+
+        auxvars = split(br_params["aux"])
+        aux_avail = [0,                # η_total
+                     0, 0, 0,  # ηx, ηy, ηz
+                     0, 0, 0,  # Jx, Jy, Jz
+                     0, 0, 0,  # Ex, Ey, Ez
+                     ]
+        for i = 1:length(auxvars)
+            if auxvars[i] == "eta_total"
+                aux_avail[1] = i
+            elseif auxvars[i] == "etax"
+                aux_avail[3] = i
+            elseif auxvars[i] == "etay"
+                aux_avail[4] = i
+            elseif auxvars[i] == "etaz"
+                aux_avail[2] = i
+            elseif auxvars[i] == "ix"
+                aux_avail[5] = i
+            elseif auxvars[i] == "iy"
+                aux_avail[6] = i
+            elseif auxvars[i] == "iz"
+                aux_avail[7] = i
+            elseif auxvars[i] == "ex"
+                aux_avail[8] = i
+            elseif auxvars[i] == "ey"
+                aux_avail[9] = i
+            elseif auxvars[i] == "ez"
+                aux_avail[10] = i
+            end
+        end
+
+        #-----------------------------------------------------------------------
+        # Allocate memory for simple variables
+        meshsize = (br_mesh.mx, br_mesh.my, br_mesh.mz)
+
+        # Density
+        ρ  = br_snap[:,:,:,1]
+        # Bulk momentum
+        px = br_snap[:,:,:,2]
+        py = br_snap[:,:,:,3]
+        pz = br_snap[:,:,:,4]
+        # Magnetic field
+        bx = br_snap[:,:,:,6]
+        by = br_snap[:,:,:,7]
+        bz = br_snap[:,:,:,8]
+
+        dx = diff(br_mesh.x)
+        dy = diff(br_mesh.y)
+        dz = diff(br_mesh.z)
+
+        domain = [br_mesh.x[1] br_mesh.x[end]
+                  br_mesh.y[1] br_mesh.y[end]
+                  br_mesh.z[1] br_mesh.z[end]]
+
+        pbc_x = Bool(br_params["periodic_x"])
+        pbc_y = Bool(br_params["periodic_y"])
+        pbc_z = Bool(br_params["periodic_z"])
+
+        #-----------------------------------------------------------------------
+        # Destagger and scale variables to SI-units
+        #
+        # Vector quiantities are defined at the faces, while scalar quantities
+        # are defined at cell centres. To get the bulk velcity at the centre
+        # one thus have to interpolate the momentum to the cell centres.
+        #
+        # br_xup moves values half a grid in the x-direction.
+        # params["u_u"] scales velocity from model/code-units to CGS units. 
+        # params["u_b"] scales magnetic field from model/code-units to CGS units. 
+        # cgs2SI_u scales velocity from CGS-units to SI-units
+        # cgs2SI_b scales magnetic field from CGS-units to SI-units
+        code2cgs_u = wpFloat(br_params["u_u"])
+        code2cgs_B = wpFloat(br_params["u_B"])
+
+        ux_cgs = code2cgs_u * br_xup(px, pbc_x) ./ ρ
+        uy_cgs = code2cgs_u * br_yup(py, pbc_y) ./ ρ
+        uz_cgs = code2cgs_u * br_zup(pz, pbc_z) ./ ρ
+        ux_SI = ux_cgs * Constants.cgs2SI_u
+        uy_SI = uy_cgs * Constants.cgs2SI_u
+        uz_SI = uz_cgs * Constants.cgs2SI_u
+
+        # De-stagger and scale magnetic field
+        # bx_SI = bx_cgs * Constants.cgs2SI_b
+        # by_SI = by_cgs * Constants.cgs2SI_b
+        # bz_SI = bz_cgs * Constants.cgs2SI_b
+        # Only scale magnetic field
+        bx_SI = code2cgs_B * Constants.cgs2SI_b * bx
+        by_SI = code2cgs_B * Constants.cgs2SI_b * by
+        bz_SI = code2cgs_B * Constants.cgs2SI_b * bz
+        
+        #-----------------------------------------------------------------------
+        # Compute the electric field and current density
+        eField  = zeros(wpFloat, 3, meshsize...)
+        calcEfield = true
+        resistiveMHD = true
+
+        if (aux_avail[8] > 0) & (aux_avail[9] > 0) & (aux_avail[10] > 0)
+            eField[1,:,:,:] = br_aux[:,:,:,aux_avail[8]]
+            eField[2,:,:,:] = br_aux[:,:,:,aux_avail[9]]
+            eField[3,:,:,:] = br_aux[:,:,:,aux_avail[10]]
+            calcEfield = false
+        elseif  (aux_avail[2] > 0) & (aux_avail[3] > 0) & (aux_avail[4] > 0)
+            # Calculate η_total? or J using components?
+            println(string("Error: Computing E-field using η-components not ",
+                           "implemented"))
+            return
+        elseif aux_avail[1] > 0
+            η = br_aux[:,:,:,aux_avail[1]]
+            # Do nothing
+        else
+            println("Warning: No resisstance nor electric field in aux-vars.")
+            println("         Proceeds assuming ideal MHD.")
+            resistiveMHD = false
+        end
+
+        # Bring magnetic field to cell centres
+        bField  = zeros(wpFloat, 3, meshsize...)
+        bField[1,:,:,:] = br_xup(bx_SI, pbc_x)
+        bField[2,:,:,:] = br_xup(by_SI, pbc_y)
+        bField[3,:,:,:] = br_xup(bz_SI, pbc_z)
+
+#        # Temporary treatment of 2D meshes. Necessary for trilinear interpolations.
+#        # Idealy, you want a bilinear_xz interpolation function to avoid
+#        # interpolation in the third dimension.
+#        if length(br_mesh.y) == 1
+#            br_mesh.y = [br_mesh.y[1], br_mesh.y[1]]
+#        end
+
+        if calcEfield
+            # This allocation is not needed if cross() allows components and
+            # not only the vectors.
+            bulkvel = zeros(wpFloat, 3, meshsize...)
+            bulkvel[1,:,:,:] = ux_SI
+            bulkvel[2,:,:,:] = uy_SI
+            bulkvel[3,:,:,:] = uz_SI
+            if resistiveMHD
+                # Calculate current density
+                if (aux_avail[5] > 0) & (aux_avail[6] > 0) & (aux_avail[7] > 0)
+                    # Current density available in aux-variables
+                    J = zeros(wpFLoat, 3, meshsize...)
+                    J[1,:,:,:] = br_aux[:,:,:,aux_avail[5]]
+                    J[2,:,:,:] = br_aux[:,:,:,aux_avail[6]]
+                    J[3,:,:,:] = br_aux[:,:,:,aux_avail[7]]
+                elseif aux_avail[1] > 0
+                    # Calculate J using the curl of the magnetic field.
+                    # Use magnetic field on cell faces and upwind scheme
+                    J = Schemes.curl(bx_SI, by_SI, bz_SI,
+                                     br_mesh.x, br_mesh.y, br_mesh.z,
+                                     Schemes.derivateUpwind
+                                     )/Constants.μ_0
+                end
+                eField = J - cross(bulkvel, bField)
+                eField[1,:,:,:] .*= η
+                eField[2,:,:,:] .*= η
+                eField[3,:,:,:] .*= η
+            else
+                eField = -cross(bulkvel, bField)
+            end
+        end
+
+        #-----------------------------------------------------------------------
+        # Define/compute other parameters/variables.
+
+        ∇B, ∇b̂, ∇ExB = compute∇s(bField, eField,
+                                 br_mesh.x,
+                                 br_mesh.y,
+                                 br_mesh.z,
+                                 derivateUpwind,
+                                 )
+
+        #-------------------------------------------------------
+
+        return new(bField, eField, ∇B, ∇b̂, ∇ExB,
+                   br_mesh.x, br_mesh.y, br_mesh.z,
+                   domain, numdims)
+    end # constructor Mesh
+
 end # Struc
 
 #-------------------#
@@ -167,9 +378,37 @@ function compute∇s(bField ::Array{wpFloat, 4},
             end
         end
     end
-    ∇B = ∇(BB,  dx, dy, dz, derivate4thOrder) 
+    ∇B = ∇(BB, dx, dy, dz, derivate4thOrder) 
     ∇b̂ = ∇(b̂,  dx, dy, dz, derivate4thOrder)
     ∇ExBdrift= ∇(ExBdrift, dx, dy, dz, derivate4thOrder)
+    return ∇B, ∇b̂, ∇ExBdrift
+end # function compute∇s
+#|
+function compute∇s(bField ::Array{wpFloat, 4},
+                   eField ::Array{wpFloat, 4},
+                   xCoords::Vector{wpFloat},
+                   yCoords::Vector{wpFloat},
+                   zCoords::Vector{wpFloat},
+                   scheme ::Function
+                   )
+    _, nx, ny, nz = size(bField)
+    BB = norm4(bField)
+    b̂ = zeros(wpFloat, 3, nx, ny, nz)
+    ExBdrift = zeros(wpFloat, 3, nx, ny, nz)
+    for i = 1:nx
+        for j= 1:ny
+            for k = 1:nz
+                B⃗ = bField[:,i,j,k]
+                E⃗ = eField[:,i,j,k]
+                B = BB[i,j,k]
+                b̂[:,i,j,k]  .= B⃗ ./ B
+                ExBdrift[:,i,j,k] .= (E⃗ × B⃗) ./ B^2
+            end
+        end
+    end
+    ∇B = ∇(BB, xCoords, yCoords, zCoords, scheme) 
+    ∇b̂ = ∇(b̂,  xCoords, yCoords, zCoords, scheme)
+    ∇ExBdrift= ∇(ExBdrift, xCoords, yCoords, zCoords, scheme)
     return ∇B, ∇b̂, ∇ExBdrift
 end # function compute∇s
 #------------------#

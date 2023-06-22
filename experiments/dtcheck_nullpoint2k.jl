d5776e5d07b8465cfa4c22e458c93f227b934ab8
eilifso <e.s.oyre@astro.uio.no>
2023-06-22 17:40:48 +0200

    [ADD] Add exp.: dt-check in nullpoint2k

---
+++ experiments/dtcheck_nullpoint2k.jl
@@ -0,0 +1,51 @@
+using TraceParticles
+using LinearAlgebra
+
+# Set simulation parameters
+params = Parameters(
+    dt=1e-8,
+    nsteps=Int64(1e6),
+    npart=5,
+    tp_expname="nullpoint2k_dtrun",
+    tp_expdir="/Users/eilifo/repos/tp/data/dtrun",
+    solver="full-orbit",
+    scheme="RK4",
+    interp="bilinear_xz",
+    br_expname="nullpoint2k",
+    br_expdir="/Users/eilifo/data/ohf/nullpoint2k",
+    br_isnap=700,
+    wp_part=Float64,
+    wp_snap=Float32,
+    pos_distr="point",
+    posxbounds=[15.547e6, 15.739e6, 15.846e6, 15.947e6, 16.226e6],
+    posybounds=[1e6, 1e6, 1e6, 1e6, 1e6],
+    poszbounds=[-6.709e6, -6.519e6, -6.410e6, -6.314e6, -6.048e6],
+    vel_distr="mb",
+    pbc = (false, true, false),
+    specie=[1,1,1,1,1],
+)
+
+
+tf = 0.01
+ndt = 10
+dts = 10 .^ (LinRange(-7.33,-8,ndt))
+stepslist = round.(Integer, tf ./ dts)
+
+exp = tp_init!(params)
+for i = 1:ndt
+    params.dt = dts[i]
+    params.nsteps = stepslist[i]
+
+    # Initialise experiment
+    tp_reinit_particles!(exp)
+
+    # Run experiment
+    tp_run!(exp)
+
+    tp_savetp(exp,
+              string(params.tp_expdir, "/",
+                     params.tp_expname, "_$(dts[i]).tp")
+              )
+end
+
+    
